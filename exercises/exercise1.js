1bbe39ebc366b5d4f546682b6b238521496d9798
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-12-07 11:55:31 +0100

    part 1 solution

--- exercises/exercise1.js
+++ exercises/exercise1.js
@@ -1,7 +1,12 @@
-const { } = require('@bsv/sdk');
-
+const { PrivateKey } = require('@bsv/sdk');
 /*
     Generate a random private key and
     - print it in WIF format
     - print its address
  */
+const privKey = PrivateKey.fromRandom()
+console.log("Random private key in WIF format:", privKey.toWif())
+console.log("Address: ", privKey.toAddress())
+const pubKey = privKey.toPublicKey()
+console.log("Public key: ", pubKey.toString())
+
