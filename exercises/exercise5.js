37ce8e1c40a13ce6ba33e5a5d5340edf43ea9334
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-12-10 09:32:54 +0100

    solved

--- exercises/exercise5.js
+++ exercises/exercise5.js
@@ -1,4 +1,4 @@
-const { } = require('@bsv/sdk');
+const { P2PKH } = require('@bsv/sdk');
 
 /*
   Generate locking script for a P2PKH address
@@ -7,7 +7,7 @@ const { } = require('@bsv/sdk');
   - remember the index of that output for the next exercise (as UTXO index)
  */
 
-const address = "put_your_address_here"
+const address = "1PMT41sRmmVtC19AB4J7saeVCfATuAovnW"
 
-const lockingScript = "" // <- TODO: update this line
+const lockingScript = new P2PKH().lock(address).toASM()
 console.log("Locking script: ", lockingScript)
